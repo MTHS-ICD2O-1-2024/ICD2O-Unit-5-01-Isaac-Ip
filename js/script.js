98f8e16e0378838bd846313a0f05a39db2f2fba2
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-04-16 13:30:29 -0400

    edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved 
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created by: Isaac Ip
+// Created on: Apr 2025 
+// This file contains the JS functions for index.html 
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/** 
+* This function checks if the user has selected the random number from above 
+*/
+// eslint-disable-next-line no-unused-vars 
+function checkNumber() {
+    // input
+    const userNumber = parseInt(document.getElementById('user-number').value)
+
+    // process 
+    if (userNumber === randomNumber) {
+        // output 
+        document.getElementById('result').innerHTML =
+
+            '<p>You have guessed the correct number!</p>'
+    }
+
+    if (userNumber !== randomNumber) {
+        // output 
+        document.getElementById('result').innerHTML =
+
+            "<p>You have guessed the wrong number! The random number is: " + randomNumber + ". </p>"
+
+    }
+}
